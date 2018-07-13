a6e2a58261e0d36072a1b651badb434f1a79cea2
Andreas Braun <git@alcaeus.org>
2018-07-13 21:12:41 +0200

    Fix invalid syntax in entryscript

--- tideways-daemon/entrypoint.sh
+++ tideways-daemon/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 extra="${DAEMON_OPTIONS} --hostname=${HOSTNAME:-tideways}"
-extra+=" --env=${TIDEWAYS_ENV:-development}"
-extra+=" --address=0.0.0.0:${TIDEWAYS_PORT_TCP}"
-extra+=" --udp=0.0.0.0:${TIDEWAYS_PORT_UDP}"
+extra="${extra} --env=${TIDEWAYS_ENV:-development}"
+extra="${extra} --address=0.0.0.0:${TIDEWAYS_PORT_TCP}"
+extra="${extra} --udp=0.0.0.0:${TIDEWAYS_PORT_UDP}"
 
 /usr/bin/tideways-daemon $extra
--- tideways-proxy/entrypoint.sh
+++ tideways-proxy/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 extra="${PROXY_OPTIONS} --hostname=${HOSTNAME:-tideways}"
-extra+=" --listen=0.0.0.0:${TIDEWAYS_PORT}"
+extra="${extra} --listen=0.0.0.0:${TIDEWAYS_PORT}"
 
 /usr/bin/tideways-proxy $extra
